863058f83201da37b26b8812d445f48345f317a9
Jooseppi12 <urijozsef@europe.com>
2025-05-16 10:12:55 +0200

    Fix redirect

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,5 @@
 import {redirect} from 'next/navigation';
 
-const basePathToUse = process.env.GHREPO !== undefined ? "/" + process.env.GHREPO : "";
-
 export default function HomePage() {
-  redirect(`${basePathToUse}/docs/core`)
+  redirect(`docs/core`)
 }
